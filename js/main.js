a9ed4cb64fd5acd7b8f5032c7423a269340456c7
IlyaOrluk <misfits120@mail.ru>
2019-08-08 02:44:27 +0300

    add functional from image redactor

--- js/main.js
+++ js/main.js
@@ -1,51 +1,140 @@
-class Brick {
-    constructor(x, y, className, string){
+let aspectRatio = document.querySelector('#aspectRatio');
+aspectRatio.addEventListener('click' , () => {
+    console.log(aspectRatio.value)
+});
+
+class Image {
+    constructor(x, y, width, height, className, img) {
         this.x = x,
         this.y = y,
+        this.width = width,
+        this.height = height
         this.className = className,
-        this.brick = document.createElement('div'),
-        this.innerString = string,
-        this.mouseDown = false
+        this.image = document.createElement('div'),
+        this.resizeRightBottom = document.createElement('span'),
+        this.resizeRight = document.createElement('span'),
+        this.resizeBottom = document.createElement('span'),
+        this.img = img,
+        this.move = false,
+        this.resizeRightBottomDown = false,
+        this.resizeRightDown = false,
+        this.resizeBottomDown = false
     }
     addElement = () => {
-        this.brick.className = this.className;
-        this.brick.style.cssText = `
-            position: fixed;
+        this.image.className = this.className;
+        this.image.style.cssText = `
+            position: absolute;
+            display; flex;
+            justify-content: center;
+            align-items: center;
             padding: 1vh;
+            width: ${this.width}px;
+            height: ${this.height}px;
             top: ${this.y}px;
             left: ${this.x}px;
             border: 1px solid #000;
             user-select: none;
             cursor: pointer;
+ 
         `;
-        this.brick.innerHTML = `<strong>${this.innerString}</strong>`;
-        document.body.append(this.brick);
+        this.image.innerHTML = `<img class='image' src='${this.img}'/>`;
+        document.body.append(this.image);
+        this.image.append(this.resizeRightBottom);
+        this.resizeRightBottom.className = 'right-bottom-resize';
+        this.image.append(this.resizeRight);
+        this.resizeRight.className = 'right-resize';
+        this.image.append(this.resizeBottom);
+        this.resizeBottom.className = 'bottom-resize';
+        
+
         this.moveElement();
+        this.resizeElement();
     }
     moveElement = () => {
-        this.brick.addEventListener('mousedown', () => {
-            this.mouseDown = true;
+        this.image.addEventListener('mousedown', () => {
+            this.move = true;
+        });
+        window.addEventListener('mouseup', () => {
+            this.move = false;
+            this.resizeRightBottomDown = false;
+            this.resizeRightDown = false;
+            this.resizeBottomDown = false;
         });
 
-        window.addEventListener('mouseup', () => {
-            this.mouseDown = false;
-        });
-        
         //move element in window
         window.addEventListener('mousemove', e => {
-            if (this.mouseDown) {
-               let mousePosX = e.clientX, mousePosY = e.clientY,
-                posBrick = this.brick.getBoundingClientRect();
-                this.x = mousePosX - posBrick.width / 2;
-                this.y = mousePosY - posBrick.height / 2;
-                this.brick.style.top = `${this.y}px`;
-                this.brick.style.left = `${this.x}px`;
+            if (this.move & !this.resizeRightBottomDown &
+                !this.resizeRightDown & !this.resizeBottomDown) {
+                let mousePosX = e.clientX, mousePosY = e.clientY;
+
+                this.x = mousePosX - this.width / 2;
+                this.y = mousePosY - this.height / 2;
+                // if (mousePosX + this.width / 2 > window.innerWidth) {
+                //     this.x = window.innerWidth - 10 - this.width;
+                // } else if (mousePosX - this.width / 2 < 0) {
+                //     this.x = 0 + 10;
+                // } else if (mousePosY + this.height / 2 > window.innerHeight) {
+                //     this.y = window.innerHeight - 10 - this.height;
+                // } else if (mousePosY - this.height / 2 < 0) {
+                //     this.y = 0 + 10;
+                // }
+                this.image.style.top = `${this.y}px`;
+                this.image.style.left = `${this.x}px`;
             }
         });
     }
+    resizeElement = () => {
+        this.resizeRightBottom.addEventListener('mousedown', () => {
+            this.resizeRightBottomDown = true;
+        });
+        this.resizeRight.addEventListener('mousedown', () => {
+            this.resizeRightDown = true;
+        });
+        this.resizeBottom.addEventListener('mousedown', () => {
+            this.resizeBottomDown = true;
+        });
+
+            window.addEventListener('mousemove', e => {
+                let mousePosX = e.clientX, mousePosY = e.clientY;
+                this.width = mousePosX-(this.x-this.width/2);
+                this.height = mousePosY-(this.y-this.height/2);
+
+                if (this.resizeRightBottomDown) {
+                    this.image.style.width = `${this.width/2}px`;
+                    this.image.style.height = `${this.height/2}px`;
+                    // fixed this resizing method...
+                    // this.image.style.top = `${this.y-this.height/2}px`;
+                    // this.image.style.left = `${this.x-this.width/2}px`;
+                } else if (this.resizeRightDown) {
+                    this.image.style.width = `${this.width/2}px`;
+                }  else if (this.resizeBottomDown) {
+                    this.image.style.height = `${this.height/2}px`;
+                }
+            });
+        
+    }
 }
 
-const brick1 = new Brick(100, 100, 'brick', 'BRICK1');
-brick1.addElement();
-const brick2 = new Brick(300, 300, 'brick', 'BRICK2');
-brick2.addElement();
+let inputPosX = document.querySelector('#posX'),
+    inputPosY = document.querySelector('#posY'),
+    inputWidth = document.querySelector('#width'),
+    inputHeight = document.querySelector('#height'),
+    inputClassName = document.querySelector('#className'),
+    inputImgPath = document.querySelector('#imgPath'),
+    addImageBtn = document.querySelector('.add-image'),
+    images = [];
+
+addImageBtn.addEventListener('click', () => {
+    console.log(aspectRatio.value)
+    images.push(
+        new Image(
+            inputPosX.value,
+            inputPosY.value,
+            inputWidth.value,
+            inputHeight.value,
+            inputClassName.value,
+            inputImgPath.value
+        )
+    );
+    images[images.length-1].addElement();
+});
